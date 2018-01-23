c30424e9e47a134ba3863c8c059fa0b88b208e8a
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-01-23 13:58:52 +0000

    Add high numbers

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -20,4 +20,8 @@ describe RomanNumerals do
   it "should be able to convert 996 to CMXCVI" do
     expect(RomanNumerals.convert(996)).to eq("CMXCVI")
   end
+
+  it "should be able to convert 1999 to MCMXCIX" do
+    expect(RomanNumerals.convert(1999)).to eq("MCMXCIX")
+  end
 end
